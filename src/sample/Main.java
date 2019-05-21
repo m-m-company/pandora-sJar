0029218a951b0eb3c841ca80ecb68f91c55f5da8
MatteoLoria <mloria58@gmail.com>
2019-05-21 14:47:58 +0200

    tu non hai pushato

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,17 +2,20 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+	
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    	FXMLLoader controllerA = new FXMLLoader(getClass().getResource("sample.fxml"));
+        VBox root = controllerA.load();
+        Controller a = controllerA.getController();
+        a.init();
+        primaryStage.setTitle("Pandora's jar");
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
