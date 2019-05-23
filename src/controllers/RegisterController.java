959ca7fa46577eeacfdbca30a89c8a184a160cd1
MattNot <matteoatnotaro@outlook.it>
2019-05-23 20:03:18 +0200

    My screen is new so my skill is better

--- src/controllers/RegisterController.java
+++ src/controllers/RegisterController.java
@@ -31,6 +31,7 @@ public class RegisterController {
 	@FXML
 	private TextField email;
 	private String code;
+
 	private String generateCode() {
 		int leftLimit = 97; // letter 'a'
 		int rightLimit = 122; // letter 'z'
@@ -38,31 +39,28 @@ public class RegisterController {
 		Random random = new Random();
 		StringBuilder buffer = new StringBuilder(targetStringLength);
 		for (int i = 0; i < targetStringLength; i++) {
-			int randomLimitedInt = leftLimit + (int) 
-					random.nextFloat() * (rightLimit - leftLimit + 1);
+			int randomLimitedInt = leftLimit + (int) random.nextFloat() * (rightLimit - leftLimit + 1);
 			buffer.append((char) randomLimitedInt);
 		}
 		String generatedString = buffer.toString();
 		return generatedString;
 	}
-	private void sendEmail(){
+
+	private void sendEmail() {
 		code = generateCode();
 		Properties props = System.getProperties();
-		props.put("mail.smtps.host","smtp.gmail.com");
-		props.put("mail.smtps.auth","true");
+		props.put("mail.smtps.host", "smtp.gmail.com");
+		props.put("mail.smtps.auth", "true");
 		Session session = Session.getInstance(props, null);
 		Message msg = new MimeMessage(session);
 		try {
 			msg.setFrom(new InternetAddress("noreplypandorasjar@gmail.com"));
-			msg.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse(email.getText(), false));
+			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email.getText(), false));
 			msg.setSubject("WELCOME!");
-			msg.setText("Welcome to Pandora's Jar my dear "+username.getText()+"!"
-					+ "\n Your code is: "+code);
+			msg.setText("Welcome to Pandora's Jar my dear " + username.getText() + "!" + "\n Your code is: " + code);
 			msg.setHeader("X-Mailer", "");
 			msg.setSentDate(new Date());
-			SMTPTransport t =
-					(SMTPTransport)session.getTransport("smtps");
+			SMTPTransport t = (SMTPTransport) session.getTransport("smtps");
 			t.connect("smtp.gmail.com", "noreplypandorasjar@gmail.com", "otxpncvzndrtbaie");
 			t.sendMessage(msg, msg.getAllRecipients());
 			System.out.println("Response: " + t.getLastServerResponse());
@@ -71,19 +69,24 @@ public class RegisterController {
 			ex.printStackTrace();
 		}
 	}
+
 	private boolean testEmail(String email) {
-		return Pattern.matches("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\" +
-                ".[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", email) ;
+		return Pattern.matches("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\"
+				+ ".[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", email);
 	}
+
 	public void sendData(ActionEvent e) {
-		if(password.getText().equals(confirmPassword.getText())) {
-			if(testEmail(email.getText()) && !username.getText().equals("")) {
-                sendEmail();
-                Stage th = (Stage) username.getScene().getWindow();
-                th.close();
-            }
+		if (password.getText().equals(confirmPassword.getText())) {
+			if (testEmail(email.getText()) && !username.getText().equals("")) {
+				sendEmail();
+				if (showConfirmDialog()) {
+					Stage th = (Stage) username.getScene().getWindow();
+					th.close();
+				}
+			}
 		}
 	}
+
 	private boolean showConfirmDialog() {
 		TextInputDialog dialog = new TextInputDialog("walter");
 		dialog.setTitle("Confirm your code!");
@@ -91,16 +94,16 @@ public class RegisterController {
 		dialog.setContentText("Please enter your code:");
 		Optional<String> result;
 		do {
-		 result = dialog.showAndWait();
-		}while(!result.equals(code));
+			result = dialog.showAndWait();
+			if(!result.isPresent())
+				return false;
+		} while (!result.get().equals(code));
 		return true;
 	}
-	public void refuse(ActionEvent e) {
-        Stage th = (Stage) username.getScene().getWindow();
-        th.close();
-	}
-	public RegisterController() {
 
+	public void refuse(ActionEvent e) {
+		Stage th = (Stage) username.getScene().getWindow();
+		th.close();
 	}
 
 }
