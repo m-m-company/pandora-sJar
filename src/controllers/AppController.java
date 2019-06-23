0f6c6e0820238bcc98186c82046a1c1c29e5c742
MattNot <matteoatnotaro@gmail.com>
2019-06-23 20:26:19 +0200

    Alert and multi-games handler

--- src/controllers/AppController.java
+++ src/controllers/AppController.java
@@ -1,14 +1,5 @@
 package controllers;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Comparator;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,9 +7,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -31,6 +20,12 @@ import model.DBConnection;
 import model.Game;
 import model.Pair;
 import model.User;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Optional;
 
 public class AppController {
 
@@ -127,15 +122,15 @@ public class AppController {
 		actualGame.getRanks().sort(new Comparator<Pair<String, Integer>>() {
 			@Override
 			public int compare(Pair<String, Integer> o1, Pair<String, Integer> o2) {
-				return Integer.compare(o2.getSecond(),o1.getSecond());
+				return Integer.compare(o2.getSecond(), o1.getSecond());
 			}
 		});
 		for (int i = 0; i < 5; i++) {
 			try {
-				Label userName = new Label(i+1 + ". " + actualGame.getRanks().get(i).getFirst());
+				Label userName = new Label(i + 1 + ". " + actualGame.getRanks().get(i).getFirst());
 				Label points = new Label(actualGame.getRanks().get(i).getSecond().toString());
 				gridPane.addRow(i, userName, points);
-			} catch(IndexOutOfBoundsException a) {
+			} catch (IndexOutOfBoundsException a) {
 				i = 5;
 			}
 		}
@@ -175,7 +170,7 @@ public class AppController {
 		 * System.out.println(game.getPath()); String path =
 		 * game.getPath().substring(6); File f = new File(path); File h = new
 		 * File(f.getParent()+File.separator+"preview.mp4");
-		 * 
+		 *
 		 * Media media = null; try {
 		 * System.out.println(h.toURI().toURL().toExternalForm()); media = new
 		 * Media(h.toURI().toURL().toExternalForm()); } catch (Exception e) { // TODO
@@ -199,11 +194,11 @@ public class AppController {
 
 	@FXML
 	void play(ActionEvent e) {
-		if(actualGame != null) {
+		if (actualGame != null) {
 			String pathGame;
 			String so = System.getProperty("os.name");
-			if(so.contains("Windows"))
-				pathGame= actualGame.getPath().substring(6);
+			if (so.contains("Windows"))
+				pathGame = actualGame.getPath().substring(6);
 			else
 				pathGame = actualGame.getPath().substring(5);
 			ProcessBuilder pb = null;
@@ -219,11 +214,10 @@ public class AppController {
 					highScore = 0;
 				}
 				pb = new ProcessBuilder("java", "-jar", pathGame, "1");
-				if(highUser != null && highScore != null) {
+				if (highUser != null && highScore != null) {
 					pb.environment().put("pandoras_HighUser", highUser);
 					pb.environment().put("pandoras_HighScore", Integer.toString(highScore));
-				}
-				else {
+				} else {
 					pb.environment().put("pandoras_HighUser", "YOU");
 					pb.environment().put("pandoras_HighScore", "0");
 				}
@@ -234,8 +228,10 @@ public class AppController {
 				Process p = pb.start();
 				p.waitFor();
 				BufferedReader bf = new BufferedReader(new FileReader(pointsFile));
-				Integer points = Integer.valueOf(bf.readLine());
-				DBConnection.inst().insertPoints(actualGame, actualUser, points);
+				while (bf.ready()) {
+					Integer points = Integer.valueOf(bf.readLine());
+					DBConnection.inst().insertPoints(actualGame, actualUser, points);
+				}
 				refreshRanks();
 				PrintWriter pw = new PrintWriter(pointsFile);
 				pw.write("");
@@ -258,34 +254,43 @@ public class AppController {
 		if (event.getCode() == KeyCode.ENTER)
 			manageAccount(null);
 	}
-	
+
 	@FXML
-    void enterPlay(KeyEvent event) {
-		if(event.getCode() == KeyCode.ENTER)
+	void enterPlay(KeyEvent event) {
+		if (event.getCode() == KeyCode.ENTER)
 			play(null);
-    }
-	
+	}
+
 
 	@FXML
 	void removeGame(ActionEvent event) {
-		if(actualGame != null) {
-			try {
-				DBConnection.inst().removeGame(actualGame);
-				actualGame = null;
-				this.refreshGamesList();
-				gridPane.getChildren().clear();
-			} catch (SQLException e) {
-				e.printStackTrace();
+		if (actualGame != null) {
+			Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+			alert.setTitle("ARE YOU SURE?");
+			alert.setContentText("Are you sure to delete this game? You will LOSE EVERY RANKS and this action is " +
+					"IRREVERSIBLE");
+			Optional<ButtonType> result = alert.showAndWait();
+			if (result.isPresent()) {
+				if (result.get().equals(ButtonType.OK)) {
+					try {
+						DBConnection.inst().removeGame(actualGame);
+						actualGame = null;
+						this.refreshGamesList();
+						gridPane.getChildren().clear();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		}
 	}
-	
-	
-	@FXML
-    void enterRemoveGame(KeyEvent event) {
-		if(event.getCode() == KeyCode.ENTER)
-			removeGame(null);
-    }
 
-	
+
+	@FXML
+	void enterRemoveGame(KeyEvent event) {
+		if (event.getCode() == KeyCode.ENTER)
+			removeGame(null);
+	}
+
+
 }
